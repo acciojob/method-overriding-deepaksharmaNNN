4186c30ff2b2093c28239a39255ce887c1801deb
Deepak Sharma <dsharma2828@gmail.com>
2023-12-15 13:25:49 +0530

    Overriding assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        B objB = new B();
+
+        // Calling the method from class A
+        String resultFromA = objB.meth();
+        System.out.println(resultFromA);
+
+        // Calling the overridden method from class B
+        String resultFromB = objB.meth();
+        System.out.println(resultFromB);
+    }
   
 }
